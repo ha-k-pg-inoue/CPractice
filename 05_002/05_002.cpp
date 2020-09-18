9d6164a01d5c1678b95a58ad4d89d37e611093e1
MSI\mukib <mukibutu.inue@gmail.com>
2020-09-18 13:59:01 +0900

    05_002完成

--- 05_002/05_002.cpp
+++ 05_002/05_002.cpp
@@ -4,10 +4,22 @@
 
 int main()
 {
-	char alphabet[26] =
-	{ 0 ,1 , 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15,
-	 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
-	};
+	// 文字も内部的には整数で管理されていることを確認する
+	char tmp[12] = "Hello World";
+	for (int i = 0; i < 12; i++)
+	{
+		printf("char %c = %d\n", tmp[i], tmp[i]);
+	}
+
+	printf("----------------------\n");
+
+	char alphabet[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+		                  'o','p','q','r','s','t','u','v','w','x','y','z' };
+	
+	for (int i = 0; i <= 25; i++)
+	{
+		printf("%c", alphabet[i]);
+	}
 
 	system("pause");
 	return 0;
