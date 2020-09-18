b9fe5267e6a05c66d3ad710fd757cd21c1f1ee5e
MSI\mukib <mukibutu.inue@gmail.com>
2020-09-18 16:34:01 +0900

    値の入れ替え

--- 07_003/07_003.cpp
+++ 07_003/07_003.cpp
@@ -1,13 +1,29 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 
+void Swap(int*, int*);
 
 int main()
 {
+	int x = 10;
+	int y = 20;
 	
+	Swap(&x, &y);
+	printf("x = %d\n", x);
+	printf("y = %d\n", y);
+
+
 
 
 	system("pause");
 	return 0;
 }
 
+void Swap(int* p1, int* p2)
+{
+	int z;
+
+	z   = *p1;
+	*p1 = *p2;
+	*p2 =   z;
+}
